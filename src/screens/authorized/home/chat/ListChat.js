811cdd14572ef802225db40504046af2ab6e6bfa
tnb107 <nguyenbathanhth@gmail.com>
2017-07-25 13:59:50 +0700

    list chat

--- src/screens/authorized/home/chat/ListChat.js
+++ src/screens/authorized/home/chat/ListChat.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class ListChat extends Component {
-    
+    static navigationOptions = {
+        title: 'List Chat'
+    }
     render() {
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
